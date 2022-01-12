57468e19e7fdb8696f4646cddfa5ad4d4f5d53b4
Cssey <casey.olawuni@etec.sp.gov.br>
2022-01-11 21:05:45 -0300

    DELETE Method Added

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -72,6 +72,21 @@ namespace Catalog.Controllers
 
             return NoContent();
         }
+
+        //DELETE /items
+        [HttpDelete("{id}")]
+        public ActionResult DeleteItem(Guid id){
+            var existingItem = repository.GetItem(id);
+
+            if(existingItem is null)
+            {
+                return NotFound();
+            }
+
+            repository.DeleteItem(id);
+            return NoContent();
+
+        }
         
     }
 
